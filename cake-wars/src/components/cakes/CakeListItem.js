d46e83401f414f641ca5fd5081fb1d0b554c78ac
Vlad Schnakovszki <shnako@yahoo.com>
2018-04-14 13:43:24 +0100

    S1: Added cake image loading and formatting.

--- cake-wars/src/components/cakes/CakeListItem.js
+++ cake-wars/src/components/cakes/CakeListItem.js
@@ -1,14 +1,19 @@
+import './CakeListItem.css';
 import React from 'react';
 
 class CakeListItem extends React.Component {
   render() {
     return (
       <a href={'/cake/' + this.props.cake.id}>
-        <div>
-          <li>{this.props.cake.name}</li>
-          <li>{this.props.cake.comment}</li>
-          <li>{this.props.cake.imageUrl}</li>
-          <li>{this.props.cake.yumFactor}</li>
+        <div className='cake-list-item col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-xs-12'>
+          <div className='pull-left col-lg-4 col-md-4 col-sm-4 col-xs-4'>
+            <img className='cake-image' src={this.props.cake.imageUrl}/>
+          </div>
+          <div>
+            <p className='cake-title'>{this.props.cake.name}</p>
+            <p className='cake-comment'>{this.props.cake.comment}</p>
+            <p>{'Yum factor: ' + this.props.cake.yumFactor}</p>
+          </div>
           <br/>
         </div>
       </a>
