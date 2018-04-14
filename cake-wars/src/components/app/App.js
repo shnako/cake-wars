2bf456dc4ebe57ed0ba081c9b9ed557e07441502
Vlad Schnakovszki <shnako@yahoo.com>
2018-04-14 18:05:33 +0100

    S2: Added navbar.

--- cake-wars/src/components/app/App.js
+++ cake-wars/src/components/app/App.js
@@ -1,22 +1,41 @@
 import './App.css';
 import React, {Component} from 'react';
-import logo from '../../logo.svg';
-import {Route, Switch} from "react-router-dom";
+import {Link, Route, Switch} from "react-router-dom";
+import {LinkContainer} from 'react-router-bootstrap';
 import Cakes from '../cakes/CakesPage'
 import CakeDetails from '../cake-details/CakeDetailsPage'
 import CakeSubmit from '../cake-submit/CakeSubmitPage'
+import {Navbar} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <a href='/'>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">Cake Wars</h1>
-          </header>
-        </a>
-        <div className="App-intro">
+      <div className="app">
+        <header>
+          <Navbar fluid>
+            <div className='navbar-header'>
+              <Link to='/' className='navbar-brand'>
+                <strong className='app-title'>Cake Wars</strong>
+              </Link>
+              <Navbar.Toggle/>
+            </div>
+            <Navbar.Collapse>
+              <ul className='nav navbar-nav navbar-nav-underline-interaction'>
+                <LinkContainer className='nav-button' exact to='/'>
+                  <li>
+                    <a>Cakes</a>
+                  </li>
+                </LinkContainer>
+                <LinkContainer className='nav-button' exact to='/cake/submit'>
+                  <li>
+                    <a>Submit</a>
+                  </li>
+                </LinkContainer>
+              </ul>
+            </Navbar.Collapse>
+          </Navbar>
+        </header>
+        <div className='app-content'>
           <Route exact path='/' component={Cakes}/>
           <Switch>
             <Route path='/cake/submit' component={CakeSubmit}/>
--- cake-wars/src/components/cakes/CakesPage.js
+++ cake-wars/src/components/cakes/CakesPage.js
@@ -24,7 +24,6 @@ class CakesPage extends React.Component {
   render() {
     return (
       <div className='col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1'>
-        <h1>Cakes</h1>
         {this.state.cakes.map(cake => <CakeListItem cake={cake} key={cake.id}/>)}
       </div>
     );
